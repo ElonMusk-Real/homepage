c2a4a54302cf461efda00455c5a65cab003a352a
Darell Hendry <1darellhendry@gmail.com>
2019-09-08 08:24:45 +0700

    adding navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Icon,
+  Typography,
+  makeStyles
+} from '@material-ui/core'
+
+
+const useStyles = makeStyles(theme => ({
+  appbar: {
+    background: 'transparent'
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+}))
+
+interface NavBarProps {
+  title?: string
+}
+
+const Navbar: React.FC<NavBarProps> = props => {
+  const classes = useStyles()
+  return (
+    <div>
+      <AppBar color='inherit' className={classes.appbar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant='h5'>
+            {props.title}
+          </Typography>
+          <IconButton size='medium'>
+            <Icon>menu</Icon>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+Navbar.defaultProps = {
+  title: 'NATA Danus'
+}
+
+export default Navbar
