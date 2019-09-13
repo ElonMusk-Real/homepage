abd27e4b1ba14f9b4e8f193f76b220428378544b
Darell Hendry <1darellhendry@gmail.com>
2019-09-13 20:22:04 +0700

    feat(COMPONENT): component product card

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Card, CardHeader, CardMedia, CardContent, Typography, Grid, Button, CardActions, CardActionArea } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  container: {
+    maxWidth: 320
+  },
+  media: {
+    height: 240,
+    width: 320
+  },
+  placeText: {
+    
+  },
+  perBox: {
+    fontSize: 18,
+    color: 'black',
+    display: 'block'
+  }
+})
+
+interface Props {
+  
+}
+
+const ProductCard: React.FC<Props> = () => {
+  const classes = useStyles()
+  const address = "Komplek BIG Kavling A no 12 Limo, Grogol, Depok";
+  console.log(address.length);
+  return (
+    <div>
+      <Card className={classes.container}>
+        <CardMedia
+          className={classes.media}
+          component="img"
+          image="https://via.placeholder.com/640x480"
+        />
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom>Kue Danus</Typography>
+            <Typography color="primary" variant='h6'>
+              <b>Rp 30.000</b>
+              <span className={classes.perBox}>per box</span>
+            </Typography>
+            <Typography className={classes.placeText} variant="caption">
+              {address.length > 40 ? `${address.substring(0, 40)}...` : address}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
+}
+
+export default ProductCard
