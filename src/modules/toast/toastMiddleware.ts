527c6d2115d6584bd869599ca4b6fa4709019b3e
Muhammad Indra Ramadhan <mindrar96@gmail.com>
2020-01-08 00:13:21 +0700

    Fix toast middleware

--- src/modules/toast/toastMiddleware.ts
+++ src/modules/toast/toastMiddleware.ts
@@ -1,9 +1,21 @@
 import { errorToast } from "./toastActions";
+import { ForbiddenError, UnauthorizedError } from "../api/error";
+import { push } from "connected-react-router";
 
-export const toastMiddleware = store => next => async action => {
+export const toastMiddleware = (store) => (next) => async (action) => {
   try {
     return await next(action);
   } catch (err) {
-    return next(errorToast(err.message));
+    if (err instanceof ForbiddenError) {
+      next(push("/"));
+      next(errorToast("Forbidden Access!"));
+    } else if (err instanceof UnauthorizedError) {
+      next(push("/login"));
+      next(errorToast(err.message));
+    } else {
+      next(errorToast(err.message));
+    }
+
+    throw err;
   }
 };
