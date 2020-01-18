5c768049ce7668edafa2cc05501f885a7a745b57
Muhammad Indra Ramadhan <mindrar96@gmail.com>
2020-01-18 13:14:18 +0700

    Clear cart after logout

--- src/modules/session/sessionAPI.ts
+++ src/modules/session/sessionAPI.ts
@@ -1,8 +1,10 @@
 import { push } from "connected-react-router";
 
 import { removeSession } from "./sessionActions";
+import { resetCart } from "../cart/cartAPI";
 
 export const logout = () => (dispatch) => {
+  dispatch(resetCart([]));
   dispatch(removeSession());
   dispatch(push("/"));
 };
